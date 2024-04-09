a072b8e5b8366b2e23283db78632308894b01c54
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-04-09 02:35:34 +0000

    update

--- src/components/MovieTrailer.jsx
+++ src/components/MovieTrailer.jsx
@@ -18,62 +18,7 @@ export default function MovieTrailer({ movie, link }) {
     />
   );
   console.log(link);
-  // const {
-  //   data: loadDatas,
-  //   isLoading,
-  //   error,
-  // } = useHttp(link, requestConfig, []);
-
-  // if (loadDatas) {
-  //   if (
-  //     !loadDatas ||
-  //     loadDatas.length === 0 ||
-  //     !loadDatas.results ||
-  //     loadDatas.results.length === 0
-  //   ) {
-  //     setGotVideo(false);
-  //   } else {
-  //     const firstMatchingVideo = loadDatas.results.find(
-  //       video.site === "YouTube" &&
-  //         (video.type === "Trailer" || video.type === "Teaser"),
-  //     );
-  //     if (!firstMatchingVideo || firstMatchingVideo.length === 0) {
-  //       setGotVideo(false);
-  //     }
-  //   }
-  // }
-
-  // if (isLoading) {
-  //   return <p className="isLoading">Fetching Trailer ...</p>;
-  // }
-
-  // if (error) {
-  //   console.log(error);
-  //   return (
-  //     <div className="movie-trailer-container">
-  //       <div>Movie Trailer-Long</div>
-  //       {img}
-  //     </div>
-  //   );
-  // }
-
-  // if (!gotVideo) {
-  //   console.log(gotVideo);
-  //   return (
-  //     <div className="movie-trailer-container">
-  //       <div>Movie Trailer-Long</div>
-  //       {img}
-  //     </div>
-  //   );
-  // }
-
-  // if (loadDatas && loadDatas.results) {
-  //   const firstMatchingVideo = loadDatas.results.find(
-  //     video.site === "YouTube" &&
-  //       (video.type === "Trailer" || video.type === "Teaser"),
-  //   );
-  //   setCurrentKey(firstMatchingVideo.key);
-  // }
+  
 
   return (
     <div className="movie-trailer-container">
--- src/components/hooks/useGetVideo.js
+++ src/components/hooks/useGetVideo.js
@@ -2,17 +2,54 @@ import { useState, useCallback, useEffect } from "react";
 import useHttp from "./useHttp";
 
 export default function useGetVideo(url, config, initialData) {
-  const [data, setData] = useState(initialData);
+  const [currentKey, setCurrentKey] = useState(null);
+  const [isLoadingVideo, setIsLoadingVideo] = useState(false);
+  const [gotVideo, setGotVideo] = useState(false);
 
   const {
-    data: loadedData,
+    data: loadedDatas,
     isLoading,
     error,
   } = useHttp(url, config, initialData);
 
+  if (loadedDatas) {
+    if (
+      !loadedDatas ||
+      loadedDatas.length === 0 ||
+      !loadedDatas.results ||
+      loadedDatas.results.length === 0
+    ) {
+      setGotVideo(false);
+    } else {
+      const firstMatchingVideo = loadedDatas.results.find(
+        video.site === "YouTube" &&
+          (video.type === "Trailer" || video.type === "Teaser"),
+      );
+      if (!firstMatchingVideo || firstMatchingVideo.length === 0) {
+        setGotVideo(false);
+      }
+    }
+  }
+
+  if (isLoading) {
+    setIsLoadingVideo(isLoading);
+  }
+
+  if (error) {
+    setGotVideo(true);
+  }
+
+  if (loadedDatas && loadedDatas.results) {
+    const firstMatchingVideo = loadedDatas.results.find(
+      video.site === "YouTube" &&
+        (video.type === "Trailer" || video.type === "Teaser"),
+    );
+    setCurrentKey(firstMatchingVideo.key);
+  }
+
   return {
-    data,
-    isLoading,
-    error,
+    currentKey,
+    isLoadingVideo,
+    gotVideo,
   };
 }
