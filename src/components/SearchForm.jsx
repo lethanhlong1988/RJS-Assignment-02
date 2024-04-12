d4cc0df614907bb622a082057309850da501f146
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-04-12 01:28:54 +0000

    update

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,23 +1,43 @@
+import { useRef, useState } from "react";
+
 import { ImSearch } from "react-icons/im";
 import "./SearchForm.css";
 import Button from "./UI/Button";
 
-export default function SearchForm() {
+export default function SearchForm({ onDataSubmit }) {
+  const [inputData, setInputData] = useState("");
+
+  function handleChange(event) {
+    event.preventDefault();
+    setInputData(event.target.value);
+  }
+
+  function handleSearch() {
+    onDataSubmit(inputData);
+  }
+
+  function handleReset() {
+    onDataSubmit("");
+    document.getElementById("input").value = "";
+  }
+
   return (
     <div className="search-form-container">
       <div className="search-table">
         <div className="input-form">
-          <input className="input-item" id="input" />
+          <input className="input-item" id="input" onChange={handleChange} />
           <label className="input-label" htmlFor="input">
             <ImSearch />
           </label>
         </div>
         <div className="custom-br"></div>
         <div className="action-form">
-          <Button textOnly className="reset-button">
+          <Button textOnly className="reset-button" onClick={handleReset}>
             Reset
           </Button>
-          <Button className="search-button">Search</Button>
+          <Button className="search-button" onClick={handleSearch}>
+            Search
+          </Button>
         </div>
       </div>
     </div>
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import "./Search.css";
 import { API_KEY } from "../../store/apiKey";
 
@@ -5,21 +7,27 @@ import Navbar from "../../components/Navbar";
 import SearchForm from "../../components/SearchForm";
 import MovieList from "../../components/MovieList";
 
-const key_word = "mickey";
+const key_word = "batman";
 const link =
   "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1";
 
 export default function Search() {
+  const [searChContent, setSearchContent] = useState("");
+  function handleSearch(data) {
+    setSearchContent(data);
+    console.log(searChContent);
+  }
+
   return (
     <div className="app">
       <div className="app-content">
         <Navbar />
-        <SearchForm />
+        <SearchForm onDataSubmit={handleSearch} />
         <div className="search-list">
           <MovieList
             className="search-list"
             name="Search Result"
-            link={`${link}&api_key=${API_KEY}&query=${key_word}`}
+            link={`${link}&api_key=${API_KEY}&query=${searChContent}`}
             view={false}
           />
         </div>
